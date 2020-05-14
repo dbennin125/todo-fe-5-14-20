d05ff08118dba5c61af4ac198f46343e47fc425a
Daniel Bennington <dbennington125@gmail.com>
2020-05-14 15:28:41 -0700

    states are working correctly

--- src/Todos.js
+++ src/Todos.js
@@ -37,17 +37,47 @@ state = {
         importance: 8,
         is_completed: true,
       }
-    ]
+    ],
+    newTodoName: '',
+    newTodoImportance: 0,
+}   
 
-}    
+handleNameChange =  (e) => {
+    this.setState({ newTodoName: e.target.value });
+}
+
+handleImportanceChange = (e) => {
+    this.setState({ newTodoImportance: e.target.value });
+}
+
+getClassName = (item) => {
+    if (item.is_completed) return 'completed';
+    if (!item.is_completed) return 'incomplete';
+}
+
+handSubmit = (e) => {
+    e.preventDefault();
     
+}
     
     render() {
-        return (
+    //   (console.log(this.state.newTodoName)) //newTodoName state works
+    //   (console.log(this.state.newTodoImportance, 'lolol')); //state changes with input from importance
+        
+      return (
             <div className="App">
+                <form onSubmit="">
+                  <label name="name">
+                      Add Name:<input value={this.state.newTodoName} onChange={this.handleNameChange} />  
+                </label>
+                <label name="importance">
+                    Add Importance Level<input value={this.state.newTodoImportance} onChange={this.handleImportanceChange}/>
+                </label>
+                <button>Add</button>
+                </form>
                <ul>
                 {
-                this.state.todos.map(item => <li className={ item.is_completed ? 'completed' : 'incomplete'} key={item.task}>
+                this.state.todos.map(item => <li className={this.getClassName(item)} key={item.task}>
                     <h4>Task: {item.task}<p>Importance: {item.importance}</p></h4>
                 </li>)
                 }
