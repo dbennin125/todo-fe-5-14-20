3aa030d239944561f619237c84cc325679255e98
Daniel Bennington <dbennington125@gmail.com>
2020-05-14 14:12:06 -0700

    hard data loaded

--- src/Todos.js
+++ src/Todos.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react'
 
+
+
+
 export default class Todos extends Component {
+//intialize state and hard data
+state = {
+    todos: [
+    {
+        id: 1,
+        task: 'walk dog',
+        importance: 10,
+        is_completed: false,
+      },
+    {
+        id: 2,
+        task: 'shopping',
+        importance: 4,
+    is_completed: true
+      },
+      {
+        id: 3,
+        task: 'get booze',
+        importance: 6,
+         is_completed: false
+      },
+    {
+        id: 4,
+        task: 'get produce',
+        importance: 8,
+         is_completed: false
+      },
+      {
+        id: 5,
+        task: 'get juice',
+        importance: 8,
+         is_completed: true
+      }
+    ]
+
+}    
+    
+    
     render() {
         return (
-            <div>
+            <div className="listContainer">
                 Good on you!
             </div>
         )
