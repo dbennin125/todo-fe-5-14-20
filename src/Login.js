606ced0a0cac22e822ff61ccc13a625fef71ccda
Daniel Bennington <dbennington125@gmail.com>
2020-05-14 18:30:51 -0700

    signup works

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
+import request from 'superagent';
 
 export default class Login extends Component {
     state = {
         email: '',
-        Password: ''
+        Password: '',
+        user: ''
     }
     
-    handSubmit = (e) => {
+    handSubmit = async (e)  => {
         e.preventDefault();
-
-        const token = Math.random();
+        const existingUser = await request.post(`http://localhost:3000/auth/signin`,{
+            email: this.state.email,
+            password: this.state.password})
+        const token = { token: existingUser.body };
         //creates a fake token, the real one comes from the backend
         console.log(token, this.state.email, this.state.password)
+        
         this.props.handleTokenChange(token);
         // handleTokenChange is in App.js this will bring it to App.js
         this.props.history.push('/todos');
@@ -19,6 +24,7 @@ export default class Login extends Component {
     }
 
     render() {
+       
         return (
             <div>
             <form>
@@ -30,7 +36,7 @@ export default class Login extends Component {
                       Password:
                       <input onChange={(e) => this.setState({ password: e.target.value })} value={this.state.password} />
                   </label>
-                  <button>Log in</button>
+                  <button>Welcome Back</button>
               </form>
           </div>
       )
